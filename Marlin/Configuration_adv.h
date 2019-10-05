fef8e881ae1ca0a10309b6b0c62b453f387854b4
Ha-Z-Z-ard <41190303+Ha-Z-Z-ard@users.noreply.github.com>
2019-10-05 14:24:00 +0100

    Tweak to Z Autoalign

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -602,8 +602,8 @@
 #define Z_STEPPER_AUTO_ALIGN
 #if ENABLED(Z_STEPPER_AUTO_ALIGN)
   // Define probe X and Y positions for Z1, Z2 [, Z3]
-  #define Z_STEPPER_ALIGN_XY { {  10, 110 }, { 210,  110 }	}  // Set number of iterations to align
-  #define Z_STEPPER_ALIGN_ITERATIONS 3
+  #define Z_STEPPER_ALIGN_XY {{  20, 110 }, { 190,  110 }}  // Set number of iterations to align
+  #define Z_STEPPER_ALIGN_ITERATIONS 5
   // Enable to restore leveling setup after operation
   #define RESTORE_LEVELING_AFTER_G34
 
